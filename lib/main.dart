28656a4ea502b68e52ee20798fac3f65b6eeb97e
justdvnsh <justdvnsh2208@gmail.com>
2020-03-25 02:03:18 +0530

    added lists and answers

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> answers = [
+    false,
+    true,
+    true
+  ];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +54,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +78,37 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  bool correctAnswer = answers[questionNumber];
+
+                  if (correctAnswer == true) {
+
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+
+                  } else {
+
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+
+                  }
+
+
+                  if (questionNumber != 2) {
+                    questionNumber += 1;
+                  } else if (questionNumber == 2) {
+                    questionNumber = 0;
+                  }
+
+                });
               },
             ),
           ),
@@ -79,12 +126,45 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+
+                  bool correctAnswer = answers[questionNumber];
+
+                  if (correctAnswer == false) {
+
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+
+                  } else {
+
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+
+                  }
+
+
+                  if (questionNumber != 2) {
+                    questionNumber += 1;
+                  } else if (questionNumber == 2) {
+                    questionNumber = 0;
+                  }
+
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
